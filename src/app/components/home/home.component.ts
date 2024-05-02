b11021a05cc4b1dc17bdbe84374e7b66578996f9
Subhrangsu90 <subhrangsubera@outlook.com>
2024-05-03 02:17:50 +0530

    sidenav partially completed

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { SidenavComponent } from '../../shared/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SidenavComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
---
+++ src/app/shared/sidenav/sidenav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sidenav',
+  standalone: true,
+  imports: [],
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.css',
+})
+export class SidenavComponent {}
