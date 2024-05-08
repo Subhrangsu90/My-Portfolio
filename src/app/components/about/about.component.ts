d4798e120b025b3ae7abbdf71b82c14367bea963
Subhrangsu90 <subhrangsubera@outlook.com>
2024-05-09 02:22:57 +0530

    stack and about page hulf done

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
-import { SidenavComponent } from '../../shared/sidenav/sidenav.component';
+import { CommonModule } from '@angular/common';
+import { ContactUsComponent } from '../../shared/contact-us/contact-us.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-about',
   standalone: true,
-  imports: [SidenavComponent],
+  imports: [CommonModule, ContactUsComponent, FooterComponent],
   templateUrl: './about.component.html',
   styleUrl: './about.component.css',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  qualifications = [
+    {
+      institution: 'Vidyasagar University, West Bengal',
+      degree: 'Master of Science',
+      field: 'Computer Science',
+      cgpa: 8.86,
+      passingYear: 2023,
+    },
+    {
+      institution: 'Prabhat Kumar College, West Bengal',
+      degree: 'Bachelor of Science',
+      field: 'Computer Science',
+      cgpa: 8.34,
+      passingYear: 2021,
+    },
+    {
+      institution: 'Barachira Prabir Banabehari High School, West Bengal',
+      degree: 'Class XII',
+      field: 'PCM and Biology',
+      percentage: 68.4,
+      passingYear: 2016,
+    },
+    {
+      institution: 'Barachira Prabir Banabehari High School, West Bengal',
+      degree: 'Class X',
+      percentage: 74.14,
+      passingYear: 2014,
+    },
+  ];
+}
--- src/app/components/stack/stack.component.ts
+++ src/app/components/stack/stack.component.ts
@@ -1,11 +1,117 @@
 import { Component } from '@angular/core';
 import { SidenavComponent } from '../../shared/sidenav/sidenav.component';
+import { CommonModule } from '@angular/common';
+import { ContactUsComponent } from '../../shared/contact-us/contact-us.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-stack',
   standalone: true,
-  imports: [SidenavComponent],
+  imports: [CommonModule, ContactUsComponent, FooterComponent],
   templateUrl: './stack.component.html',
   styleUrl: './stack.component.css',
 })
-export class StackComponent {}
+export class StackComponent {
+  stack = [
+    {
+      name: 'Languages',
+      technologies: [
+        {
+          name: 'C',
+          description:
+            'General-purpose, imperative computer programming language.',
+          useFor: 'System programming',
+        },
+        {
+          name: 'C++',
+          description:
+            'General-purpose programming language with object-oriented features.',
+          useFor:
+            'System programming, game development, performance-critical applications',
+        },
+        {
+          name: 'Java',
+          description:
+            'High-level, class-based, object-oriented programming language.',
+          useFor:
+            'Enterprise software, Android app development, web applications',
+        },
+        {
+          name: 'JavaScript',
+          description:
+            'High-level, interpreted scripting language for web development.',
+          useFor:
+            'Web development, front-end development, server-side development',
+        },
+        {
+          name: 'HTML',
+          description:
+            'Standard markup language for creating web pages and web applications.',
+          useFor: 'Structure and content of web pages',
+        },
+        {
+          name: 'CSS',
+          description:
+            'Style sheet language used for describing the presentation of a document written in HTML.',
+          useFor: 'Visual styling of web pages',
+        },
+      ],
+    },
+    {
+      name: 'Frameworks',
+      technologies: [
+        {
+          name: 'Angular',
+          description:
+            'Platform and framework for building single-page client applications using HTML and TypeScript.',
+          useFor: 'Developing robust and scalable web applications',
+        },
+        {
+          name: 'React',
+          description: 'JavaScript library for building user interfaces.',
+          useFor: 'Building interactive user interfaces',
+        },
+        {
+          name: 'Node.js',
+          description:
+            "JavaScript runtime built on Chrome's V8 JavaScript engine.",
+          useFor: 'Server-side scripting, networking applications',
+        },
+      ],
+    },
+    {
+      name: 'Databases',
+      technologies: [
+        {
+          name: 'MongoDB',
+          description:
+            'NoSQL document database for storing data in JSON-like format.',
+          useFor: 'Scalable, high-performance, high availability',
+        },
+        {
+          name: 'SQL',
+          description:
+            'Standard language for managing and manipulating relational databases.',
+          useFor: 'Data management and manipulation in relational databases',
+        },
+      ],
+    },
+    {
+      name: 'Others',
+      technologies: [
+        {
+          name: 'OOPs',
+          description:
+            "Paradigm based on the concept of 'objects' which contain data and methods.",
+          useFor: 'Object-oriented programming',
+        },
+        {
+          name: 'Data Structures and Algorithms',
+          description:
+            'Study of data structures and algorithms used in computer science.',
+          useFor: 'Efficient data organization and manipulation',
+        },
+      ],
+    },
+  ];
+}
