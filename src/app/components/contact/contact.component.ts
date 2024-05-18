83e56ea07cb45093e23c410495e3045ce1c01cb2
Subhrangsu90 <subhrangsubera@outlook.com>
2024-05-18 22:03:36 +0530

    connect page ready

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { SidenavComponent } from '../../shared/sidenav/sidenav.component';
+import { FooterComponent } from '../../shared/footer/footer.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [SidenavComponent],
+  imports: [SidenavComponent, FooterComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.css',
 })
