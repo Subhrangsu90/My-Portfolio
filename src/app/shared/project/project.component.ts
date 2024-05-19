1060697c5538e5eb893c3df18d376a4226929986
Subhrangsu90 <subhrangsubera@outlook.com>
2024-05-20 01:28:45 +0530

    card new style add

--- src/app/shared/project/project.component.ts
+++ src/app/shared/project/project.component.ts
@@ -1,5 +1,14 @@
-import { Component, Input } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  Renderer2,
+  ViewChild,
+  OnInit,
+} from '@angular/core';
 import { Project } from './projects.module';
+import VanillaTilt from 'vanilla-tilt';
 
 @Component({
   selector: 'project',
@@ -8,6 +17,27 @@ import { Project } from './projects.module';
   templateUrl: './project.component.html',
   styleUrl: './project.component.css',
 })
-export class ProjectComponent {
+export class ProjectComponent implements OnInit {
   @Input() project!: Project;
+  @ViewChild('shine')
+  shine!: ElementRef;
+  @ViewChild('projectcard') content!: ElementRef;
+  @ViewChild('featureCard') featureCard!: ElementRef;
+
+  ngOnInit(): void {
+    VanillaTilt.init(document.querySelector('.project-card') as any);
+  }
+
+  constructor(private renderer: Renderer2) {}
+
+  handleMouseMove(event: MouseEvent) {
+    const contentRect = this.content.nativeElement.getBoundingClientRect();
+    const contentOffsetLeft = contentRect.left + window.pageXOffset;
+    const contentOffsetTop = contentRect.top + window.pageYOffset;
+
+    const posX = event.clientX - contentOffsetLeft - 100;
+    const posY = event.clientY - contentOffsetTop - 100;
+    this.shine.nativeElement.style.left = `${posX}px`;
+    this.shine.nativeElement.style.top = `${posY}px`;
+  }
 }
